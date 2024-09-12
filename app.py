2df54639f7e783b31c5ca6c6cb769b6d378e005d
YingTang123 <tangyingyuzu@163.com>
2024-09-12 19:03:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,6 @@ m02 = ['Urine protein', 'Diuretic', 'MI', 'Diabetes', 'CHF', 'TNT', 'β-blocker'
 if submitted:
     if m=="**model1**":
         model = load_model(path+'/model01.h5')
-        st.write(dir(model))
         res = model.predict(loaded_scaler1.transform(pd.DataFrame([list(d1.values())[:-5]], columns=m01)))[0]
 
         if res<0.459:
