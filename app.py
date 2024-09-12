6ef25da78fb9adf688b7e4c7d6c5106e1e2b3c2a
YingTang123 <tangyingyuzu@163.com>
2024-09-12 18:53:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,8 +136,7 @@ m02 = ['Urine protein', 'Diuretic', 'MI', 'Diabetes', 'CHF', 'TNT', 'β-blocker'
 if submitted:
     if m=="**model1**":
         model = load_model(path+'/model01.h5')
-        st.write(model.predict_proba(loaded_scaler1.transform(pd.DataFrame([list(d1.values())[:-5]], columns=m01))))
-        res = model.predict_proba(loaded_scaler1.transform(pd.DataFrame([list(d1.values())[:-5]], columns=m01)))[0]
+        res = model.predict_proba(np.array(loaded_scaler1.transform(pd.DataFrame([list(d1.values())[:-5]], columns=m01))))[0]
 
         if res<0.459:
             st.info("""
